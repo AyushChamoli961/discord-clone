7df0d92875c517badb632c67b86f4dcbbbf8dce7
AyushChamoli961 <ayushchamoli001@gmail.com>
2023-11-02 03:50:17 +0530

    production server runtime bug fix

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,33 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+import { NextResponse } from "next/server";
+
+export const runtime = "nodejs";
+
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"]
+  publicRoutes: [
+    "/",
+    "/sign-in(.*)",
+    "/sign-up(.*)",
+    "/api(.*)",
+    "/api/uploadthing"
+  ],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      //  For public routes, we don't need to do anything
+      return NextResponse.next();
+    }
+
+    const url = new URL(req.nextUrl.origin);
+
+    if (!auth.userId) {
+      //  If user tries to access a private route without being authenticated,
+      //  redirect them to the sign in page
+      url.pathname = "/sign-in";
+      return NextResponse.redirect(url);
+    }
+  },
 });
- 
+
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)"],
 };
- 
